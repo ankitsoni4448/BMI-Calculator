a3e3ab8706296f3032f2114e208b62c2c8f5dba4
Ankit Soni <56527089+ankitsoni4448@users.noreply.github.com>
2023-03-31 00:05:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const btnEl = document.getElementById("btn");
 const bmiInputEl = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
 
 function calulateBMI(){
     //console.log("click");
@@ -12,7 +13,20 @@ function calulateBMI(){
 
     //console.log(bmiValue);
     bmiInputEl.value = bmiValue;
+    if(bmiValue <18.5){
+        weightConditionEl.innerText = "Under Weight";
     
+    }
+    else if(bmiValue>= 18.5 && bmiValue<=24.9){
+        weightConditionEl.innerText = "Normal Weight";
+    }
+
+    else if(bmiValue >= 25 && bmiValue <= 29.9){
+        weightConditionEl.innerText = "Over Weight";
+    }
+    else if( bmiValue >= 30){
+        weightConditionEl.innerText = "Obesity";
+    }
 
 }
 
